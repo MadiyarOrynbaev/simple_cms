ba7b04f077e5732ab9bea77b4eb05f3c480877ef
Dagar Davletov <dagar90@gmail.com>
2015-08-11 06:27:58 +0000

    Class 6

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,6 +23,29 @@ class PagesController < ApplicationController
       end
   end
   
+  def edit
+    @page = Page.find(params[:id])
+    @subjects = Subject.sorted
+  end
+    
+  def update
+    @page = Page.find(params[:id])
+    if @page.update_attributes(page_params)
+      flash[:success] = "Обновлено успешно"
+      redirect_to action: "index", subject_id: @subject.id
+    else
+      @subjects = Subject.sorted
+      render "edit"
+    end
+  end
+  
+  def destroy
+    @page = Page.find(params[:id])
+    if @page.destroy
+      redirect_to action: "index", subject_id: @subject.id
+    end
+  end
+  
   private
     def page_params
       params.require(:page).permit(:subject_id, :title, :link, :position, :visible)
